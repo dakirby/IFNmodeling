817421b22c071bf25faffd4306eb0179fdbc5457
Duncan Kirby <39995281+dakirby@users.noreply.github.com>
2018-07-31 08:34:55 -0400

    Initial commit for markov chain monte carlo algorithm to generate fitted model with prediction intervals, incorporating prior information on reaction rates.

---
+++ mcmc.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 31 08:31:37 2018
+
+@author: Duncan
+
+MCMC Implementation Using Metropolis Hastings algorithm
+"""
+
+def MCMC():
+    return 0
+
+def main():
+    MCMC()
+    
+main()
