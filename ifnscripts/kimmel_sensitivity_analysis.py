8d9d850a8a720584517fa8d28060e4d04341967d
Duncan Kirby <39995281+dakirby@users.noreply.github.com>
2019-05-08 16:45:18 -0400

    Completed kimmel_sensitivity_analysis.py

--- ifnscripts/kimmel_sensitivity_analysis.py
+++ ifnscripts/kimmel_sensitivity_analysis.py
@@ -8,7 +8,7 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import os
 from ifnclass.ifnplot import Trajectory, DoseresponsePlot
-import matplotlib.gridspec as gridspec
+from collections import OrderedDict
 
 if __name__ == '__main__':
     # Model
@@ -51,6 +51,37 @@ if __name__ == '__main__':
                                linewidth=2)
         new_fit.add_trajectory(drb60, t, 'plot', beta_palette[idx], (0, 1), 'Beta', label=str(t) +' min',
                                linewidth=2)
+
+    # Vary parameters 20%
+    for subpop_idx, subpop in enumerate([Mixed_Model.model_1, Mixed_Model.model_2]):
+        test_keys = OrderedDict([('R1', 12000.0), ('R2', 1511.1), ('S', 10000), ('ka1', 3.3e-15), ('kd1', 1), ('ka2', 1.22e-12), ('kd2', 0.015), ('ka4', 0.0003623188), ('kd4', 0.86), ('ka3', 0.0003623188), ('kd3', 1.74e-05), ('k_a1', 9.96e-14), ('k_d1', 0.03), ('k_a2', 1.328e-12), ('k_d2', 0.002), ('k_a3', 0.000362), ('k_d3', 2.4e-06), ('k_a4', 0.000362), ('k_d4', 0.228), ('kpa', 1e-06), ('kpu', 0.0011), ('kIntBasal_r1', 0.0001), ('kIntBasal_r2', 2e-05), ('krec_r1', 0.0001), ('krec_r2', 0.0001), ('kint_a', 0.00052), ('kint_b', 0.00052), ('kdeg_a', 0.0008), ('kdeg_b', 0.0008), ('krec_a1', 0.001), ('krec_a2', 0.1), ('krec_b1', 0.005), ('krec_b2', 0.05), ('kSOCS', 0.004), ('SOCSdeg', 0.0025), ('kSOCSon', 6e-07), ('kSOCSoff', 0.00055)]).keys()
+        len_test_keys = len(test_keys)
+        for p_idx, p in enumerate(test_keys):
+            print("{}%".format(subpop_idx * 50 + p_idx/len_test_keys * 100))
+            # Increase by 20%
+            subpop.set_parameters({p: subpop.parameters[p] * 1.2})
+            response = IfnData('custom', df=Mixed_Model.mixed_dose_response(times, 'TotalpSTAT', 'Ia', list(logspace(1, 5.2)),
+                                            parameters={'Ib': 0}, sf=scale_factor),
+                               conditions={'Alpha': {'Ib': 0}})
+            new_fit.add_trajectory(response, t, 'plot', alpha_palette[0], (0, 0), 'Alpha', label='', linewidth=2, alpha=0.2)
+            response = IfnData('custom', df=Mixed_Model.mixed_dose_response(times, 'TotalpSTAT', 'Ib', list(logspace(-1, 4)),
+                                            parameters={'Ia': 0}, sf=scale_factor),
+                               conditions={'Beta': {'Ia': 0}})
+            new_fit.add_trajectory(response, t, 'plot', beta_palette[0], (0, 0), 'Beta', label='', linewidth=2, alpha=0.2)
+            # Decrease by 20%
+            subpop.set_parameters({p: subpop.parameters[p] * 0.666})
+            response = IfnData('custom',
+                               df=Mixed_Model.mixed_dose_response(times, 'TotalpSTAT', 'Ia', list(logspace(1, 5.2)),
+                                                                  parameters={'Ib': 0}, sf=scale_factor),
+                               conditions={'Alpha': {'Ib': 0}})
+            new_fit.add_trajectory(response, t, 'plot', alpha_palette[0], (0, 0), 'Alpha', label='', linewidth=2,
+                                   alpha=0.2)
+            response = IfnData('custom',
+                               df=Mixed_Model.mixed_dose_response(times, 'TotalpSTAT', 'Ib', list(logspace(-1, 4)),
+                                                                  parameters={'Ia': 0}, sf=scale_factor),
+                               conditions={'Beta': {'Ia': 0}})
+            new_fit.add_trajectory(response, t, 'plot', beta_palette[0], (0, 0), 'Beta', label='', linewidth=2,
+                                   alpha=0.2)
 
     # Aesthetics
     new_fit.axes[0].set_title(r'IFN$\alpha$')
