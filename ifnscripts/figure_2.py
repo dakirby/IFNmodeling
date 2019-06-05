1de39d4fc53836a7e19609972414a03616faed17
Duncan Kirby <39995281+dakirby@users.noreply.github.com>
2019-06-05 16:12:21 -0400

    Clarified changes to model parameters from lit values

--- ifnscripts/figure_2.py
+++ ifnscripts/figure_2.py
@@ -191,10 +191,9 @@ if __name__ == '__main__':
     # ----------------------------
     # Model
     # Parameters found by stepwise fitting GAB mean data
-    initial_parameters = {'k_a1': 4.98E-14 * 2, 'k_a2': 8.30e-13 * 2, 'k_d4': 0.0228,
+    initial_parameters = {'k_a1': 4.98E-14 * 2, 'k_a2': 8.30e-13 * 2, 'k_d4': 0.006 * 3.8,
                        'kSOCSon': 8e-07, 'kpu': 0.0011,
-                       'ka1': 3.3e-15, 'ka2': 1.22e-12, 'kd4': 0.86,
-                       'kd3': 1.74e-05,
+                       'ka2': 4.98e-13 * 2.45, 'kd4': 0.3 * 2.867,
                        'kint_a': 0.000124, 'kint_b': 0.00086,
                        'krec_a1': 0.0028, 'krec_a2': 0.01, 'krec_b1': 0.005, 'krec_b2': 0.05}
     dual_parameters = {'kint_a': 0.00052, 'kSOCSon': 6e-07, 'kint_b': 0.00052, 'krec_a1': 0.001, 'krec_a2': 0.1,
