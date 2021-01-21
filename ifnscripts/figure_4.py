5e1aca4ec2e9e6eb2c755c93deb0fa0cdf888769
dakirby <39995281+dakirby@users.noreply.github.com>
2021-01-21 17:01:02 -0500

    Added timecourse to Figure 4

--- ifnscripts/figure_4.py
+++ ifnscripts/figure_4.py
@@ -1,7 +1,7 @@
-from ifnclass.ifnmodel import IfnModel
 from ifnclass.ifnfit import DualMixedPopulation
-from ifnclass.ifndata import IfnData
-from numpy import linspace, logspace
+from ifnclass.ifndata import IfnData, DataAlignment
+from ifnclass.ifnplot import TimecoursePlot
+from numpy import logspace
 import seaborn as sns
 import matplotlib
 matplotlib.rc('xtick', labelsize=14)
@@ -16,197 +16,131 @@ import os
 from scipy.optimize import curve_fit
 from ifnclass.ifnplot import DoseresponsePlot
 
+# ---------------------------------------------------
+# First make the figure where we increase K4
+# ---------------------------------------------------
+def increase_K4_figure():
+    alpha_palette = sns.color_palette("Reds", 6)
+    beta_palette = sns.color_palette("Greens", 6)
+
+    # --------------------
+    # Set up Model
+    # --------------------
+    # Parameters found by stepwise fitting GAB mean data
+    initial_parameters = {'k_a1': 4.98E-14 * 2, 'k_a2': 8.30e-13 * 2, 'k_d4': 0.006 * 3.8,
+                          'kpu': 0.00095,
+                          'ka2': 4.98e-13 * 2.45, 'kd4': 0.3 * 2.867,
+                          'kint_a': 0.000124, 'kint_b': 0.00086,
+                          'krec_a1': 0.0028, 'krec_a2': 0.01, 'krec_b1': 0.005, 'krec_b2': 0.05}
+    dual_parameters = {'kint_a': 0.00052, 'kSOCSon': 6e-07, 'kint_b': 0.00052, 'krec_a1': 0.001, 'krec_a2': 0.1,
+                       'krec_b1': 0.005, 'krec_b2': 0.05}
+    scale_factor = 1.227
+
+    Mixed_Model = DualMixedPopulation('Mixed_IFN_ppCompatible', 0.8, 0.2)
+    Mixed_Model.model_1.set_parameters(initial_parameters)
+    Mixed_Model.model_1.set_parameters(dual_parameters)
+    Mixed_Model.model_1.set_parameters({'R1': 12000.0, 'R2': 1511.1})
+    Mixed_Model.model_2.set_parameters(initial_parameters)
+    Mixed_Model.model_2.set_parameters(dual_parameters)
+    Mixed_Model.model_2.set_parameters({'R1': 6755.56, 'R2': 1511.1})
+
+    dose_list = list(logspace(-2, 8, num=35))
+
+    dr_curve_a = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ia', dose_list,
+                                                                     parameters={'Ib': 0}, sf=scale_factor).values]
+
+    dr_curve_b = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ib', dose_list,
+                                                                     parameters={'Ia': 0}, sf=scale_factor).values]
+
+    # Now compute the 15* refractory response
+    k4sf1 = 15
+    kd4_reference = Mixed_Model.model_1.parameters['kd4']
+    kd3_reference = Mixed_Model.model_1.parameters['kd3']
+    k_d4_reference = Mixed_Model.model_1.parameters['k_d4']
+    k_d3_reference = Mixed_Model.model_1.parameters['k_d3']
+    Mixed_Model.set_global_parameters({'kd4': kd4_reference*k4sf1, 'k_d4': k_d4_reference*k4sf1})
+
+    dr_curve_a15 = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ia', dose_list,
+                                                                     parameters={'Ib': 0}, sf=scale_factor).values]
+    dr_curve_b15 = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ib', dose_list,
+                                                                     parameters={'Ia': 0}, sf=scale_factor).values]
+
+    # Plot
+    fig, axes = plt.subplots(nrows=1, ncols=1)
+    fig.set_size_inches(16, 8)
+    axes.set_xlabel("Dose (pM)", fontsize=14)
+    axes.set_title("Relative Refractory Response", fontsize=16)
+    axes.set_ylabel("pSTAT1 Relative to Primary Response", fontsize=14)
+    axes.set(xscale='log', yscale='linear')
+
+    axes.plot(dose_list, np.divide(dr_curve_a15, dr_curve_a), label=r'IFN$\alpha$ $K_{D4}\times$'+'{}'.format(k4sf2), color=alpha_palette[4], linestyle='--', linewidth=2)
+
+    axes.plot(dose_list, np.divide(dr_curve_b15, dr_curve_b), label=r'IFN$\beta$ $K_{D4}\times$'+'{}'.format(k4sf2), color=beta_palette[4], linestyle='--', linewidth=2)
+
+    axes.legend(loc=2, prop={'size': 8})
+    fig.set_size_inches(8, 8)
+    fig.savefig(os.path.join(os.getcwd(), 'results', 'Figures', 'Figure_4', 'Figure_4_Refractoriness_By_K4.pdf'))
+
+    # Also plot absolute curves
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=[6.4, 6.4])
+
+    axes.set_xlabel("Dose (pM)", fontsize=18)
+
+    axes.set_ylabel("pSTAT1", fontsize=18)
+    axes.set(xscale='log', yscale='linear')
+
+    axes.plot(dose_list, dr_curve_a, label=r'IFN$\alpha$', color=alpha_palette[5], linewidth=4)
+    axes.plot(dose_list, dr_curve_a15, label=r'IFN$\alpha$ $K_{D4}\times$' + '{}'.format(k4sf1), color=alpha_palette[5], linewidth=4, linestyle='dashed')
+
+
+    axes.plot(dose_list, dr_curve_b, label=r'IFN$\beta$', color=beta_palette[5], linewidth=4)
+    axes.plot(dose_list, dr_curve_b15, label=r'IFN$\beta$ $K_{D4}\times$' + '{}'.format(k4sf1), color=beta_palette[5], linewidth=4, linestyle='dashed')
+
+
+    axes.legend(loc=2, prop={'size': 12})
+
+    for tick in axes.xaxis.get_major_ticks():
+        tick.label.set_fontsize(18)
+    for tick in axes.yaxis.get_major_ticks():
+        tick.label.set_fontsize(18)
+    fig.savefig(os.path.join(os.getcwd(), 'results', 'Figures', 'Figure_4', 'Figure_4_Refractoriness_By_K4_Absolute.pdf'))
+
+
+def timecourse_figure():
+    # Get experimental data
+    newdata_1 = IfnData("20190108_pSTAT1_IFN_Bcell")
+    newdata_2 = IfnData("20190119_pSTAT1_IFN_Bcell")
+    newdata_3 = IfnData("20190121_pSTAT1_IFN_Bcell")
+    newdata_4 = IfnData("20190214_pSTAT1_IFN_Bcell")
+
+    # Aligned data, to get scale factors for each data set
+    alignment = DataAlignment()
+    alignment.add_data([newdata_4, newdata_3, newdata_2, newdata_1])
+    alignment.align()
+    alignment.get_scaled_data()
+    mean_data = alignment.summarize_data()
+
+    # Plot
+    green = sns.color_palette("deep")[2]
+    red = sns.color_palette("deep")[3]
+    light_green = sns.color_palette("pastel")[2]
+    light_red = sns.color_palette("pastel")[3]
+    plot = TimecoursePlot((1, 1))
+    plot.add_trajectory(mean_data, 'errorbar', 'o--', (0, 0), label=r'10 pM IFN$\alpha$2', color=light_red, dose_species='Alpha', doseslice=10.0, alpha=0.5)
+    plot.add_trajectory(mean_data, 'errorbar', 'o--', (0, 0), label=r'6 pM IFN$\beta$', color=light_green, dose_species='Beta', doseslice=6.0, alpha=0.5)
+    plot.add_trajectory(mean_data, 'errorbar', 'o-', (0, 0), label=r'3000 pM IFN$\alpha$2', color=red, dose_species='Alpha', doseslice=3000.0)
+    plot.add_trajectory(mean_data, 'errorbar', 'o-', (0, 0), label=r'2000 pM IFN$\beta$', color=green, dose_species='Beta', doseslice=2000.0)
+    fname = os.path.join(os.getcwd(), 'results', 'Figures', 'Figure_4', 'Timecourse.pdf')
+    plot.axes.set_ylabel('pSTAT1 (MFI)')
+    plot.show_figure(show_flag=False, save_flag=True, save_dir=fname)
+
 
 if __name__ == '__main__':
-    # ---------------------------------------------------
-    # First make the figure where we increase K4
-    # ---------------------------------------------------
-    def increase_K4_figure():
-        alpha_palette = sns.color_palette("Reds", 6)
-        beta_palette = sns.color_palette("Greens", 6)
-
-        # --------------------
-        # Set up Model
-        # --------------------
-        # Parameters found by stepwise fitting GAB mean data
-        initial_parameters = {'k_a1': 4.98E-14 * 2, 'k_a2': 8.30e-13 * 2, 'k_d4': 0.006 * 3.8,
-                              'kpu': 0.00095,
-                              'ka2': 4.98e-13 * 2.45, 'kd4': 0.3 * 2.867,
-                              'kint_a': 0.000124, 'kint_b': 0.00086,
-                              'krec_a1': 0.0028, 'krec_a2': 0.01, 'krec_b1': 0.005, 'krec_b2': 0.05}
-        dual_parameters = {'kint_a': 0.00052, 'kSOCSon': 6e-07, 'kint_b': 0.00052, 'krec_a1': 0.001, 'krec_a2': 0.1,
-                           'krec_b1': 0.005, 'krec_b2': 0.05}
-        scale_factor = 1.227
-
-        Mixed_Model = DualMixedPopulation('Mixed_IFN_ppCompatible', 0.8, 0.2)
-        Mixed_Model.model_1.set_parameters(initial_parameters)
-        Mixed_Model.model_1.set_parameters(dual_parameters)
-        Mixed_Model.model_1.set_parameters({'R1': 12000.0, 'R2': 1511.1})
-        Mixed_Model.model_2.set_parameters(initial_parameters)
-        Mixed_Model.model_2.set_parameters(dual_parameters)
-        Mixed_Model.model_2.set_parameters({'R1': 6755.56, 'R2': 1511.1})
-
-        dose_list = list(logspace(-2, 8, num=35))
-
-        dr_curve_a = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ia', dose_list,
-                                                                         parameters={'Ib': 0}, sf=scale_factor).values]
-
-        dr_curve_b = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ib', dose_list,
-                                                                         parameters={'Ia': 0}, sf=scale_factor).values]
-
-        # Now compute the 20* refractory response
-        k4sf1 = 15
-        kd4_reference = Mixed_Model.model_1.parameters['kd4']
-        kd3_reference = Mixed_Model.model_1.parameters['kd3']
-        k_d4_reference = Mixed_Model.model_1.parameters['k_d4']
-        k_d3_reference = Mixed_Model.model_1.parameters['k_d3']
-        Mixed_Model.set_global_parameters({'kd4': kd4_reference*k4sf1, 'k_d4': k_d4_reference*k4sf1})#,
-                                           #'kd3': kd3_reference * k4sf1, 'k_d3': k_d3_reference * k4sf1})
-
-        dr_curve_a15 = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ia', dose_list,
-                                                                         parameters={'Ib': 0}, sf=scale_factor).values]
-        dr_curve_b15 = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ib', dose_list,
-                                                                         parameters={'Ia': 0}, sf=scale_factor).values]
-        # Now compute the 60* refractory response
-        k4sf2 = 60
-        Mixed_Model.set_global_parameters({'kd4': kd4_reference*k4sf2, 'k_d4': k_d4_reference*k4sf2})#,
-                                           #'kd3': kd3_reference * k4sf2, 'k_d3': k_d3_reference * k4sf2})
-
-        dr_curve_a60 = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ia', dose_list,
-                                                                         parameters={'Ib': 0}, sf=scale_factor).values]
-        dr_curve_b60 = [el[0][0] for el in Mixed_Model.mixed_dose_response([60], 'TotalpSTAT', 'Ib', dose_list,
-                                                                         parameters={'Ia': 0}, sf=scale_factor).values]
-
-        # Plot
-        fig, axes = plt.subplots(nrows=1, ncols=1)
-        fig.set_size_inches(16, 8)
-        axes.set_xlabel("Dose (pM)", fontsize=14)
-        axes.set_title("Relative Refractory Response", fontsize=16)
-        axes.set_ylabel("pSTAT1 Relative to Primary Response", fontsize=14)
-        axes.set(xscale='log', yscale='linear')
-        # axes.plot(dose_list, np.divide(dr_curve_a15, dr_curve_a), label=r'IFN$\alpha$ $K_{D4}\times$'+'{}'.format(k4sf1), color=alpha_palette[4], linestyle='-', linewidth=2)
-        axes.plot(dose_list, np.divide(dr_curve_a60, dr_curve_a), label=r'IFN$\alpha$ $K_{D4}\times$'+'{}'.format(k4sf2), color=alpha_palette[4], linestyle='--', linewidth=2)
-        # axes.plot(dose_list, np.divide(dr_curve_b15, dr_curve_b), label=r'IFN$\beta$ $K_{D4}\times$'+'{}'.format(k4sf1), color=beta_palette[4], linestyle='-', linewidth=2)
-        axes.plot(dose_list, np.divide(dr_curve_b60, dr_curve_b), label=r'IFN$\beta$ $K_{D4}\times$'+'{}'.format(k4sf2), color=beta_palette[4], linestyle='--', linewidth=2)
-
-        axes.legend(loc=2, prop={'size': 8})
-        fig.set_size_inches(8, 8)
-        fig.savefig(os.path.join(os.getcwd(), 'results', 'Figures', 'Figure_4', 'Figure_4_Refractoriness_By_K4.pdf'))
-
-        # Also plot absolute curves
-        fig, axes = plt.subplots(nrows=1, ncols=1, figsize=[6.4, 6.4])
-        #fig.set_size_inches(16, 8)
-        axes.set_xlabel("Dose (pM)", fontsize=18)
-        #axes.set_title("Absolute Refractory Response", fontsize=18)
-        axes.set_ylabel("pSTAT1", fontsize=18)
-        axes.set(xscale='log', yscale='linear')
-
-        axes.plot(dose_list, dr_curve_a, label=r'IFN$\alpha$', color=alpha_palette[5], linewidth=4)
-        axes.plot(dose_list, dr_curve_a15, label=r'IFN$\alpha$ $K_{D4}\times$' + '{}'.format(k4sf1), color=alpha_palette[5], linewidth=4, linestyle='dashed')
-        # axes.plot(dose_list, dr_curve_a60, label=r'IFN$\alpha$ $K_{D4}\times$' + '{}'.format(k4sf2), color=alpha_palette[5], linestyle='dashed', linewidth=4)
-
-        axes.plot(dose_list, dr_curve_b, label=r'IFN$\beta$', color=beta_palette[5], linewidth=4)
-        axes.plot(dose_list, dr_curve_b15, label=r'IFN$\beta$ $K_{D4}\times$' + '{}'.format(k4sf1), color=beta_palette[5], linewidth=4, linestyle='dashed')
-        # axes.plot(dose_list, dr_curve_b60, label=r'IFN$\beta$ $K_{D4}\times$' + '{}'.format(k4sf2), color=beta_palette[5], linestyle='dashed', linewidth=4)
-
-        axes.legend(loc=2, prop={'size': 12})
-        # fig.set_size_inches(8, 8)
-        for tick in axes.xaxis.get_major_ticks():
-            tick.label.set_fontsize(18)
-        for tick in axes.yaxis.get_major_ticks():
-            tick.label.set_fontsize(18)
-        fig.savefig(os.path.join(os.getcwd(), 'results', 'Figures', 'Figure_4', 'Figure_4_Refractoriness_By_K4_Absolute.pdf'))
-
-    # -----------------------------------------------------------------------
-    # Finally, make a figure about how EC50 changes as a function of K3 & K4
-    # -----------------------------------------------------------------------
-    def ec50_vs_K3_figure():
-        alpha_palette = sns.color_palette("Reds", 6)
-        beta_palette = sns.color_palette("Greens", 6)
-
-        # --------------------
-        # Set up Model
-        # --------------------
-        # Parameters found by stepwise fitting GAB mean data
-        initial_parameters = {'k_a1': 4.98E-14 * 2, 'k_a2': 8.30e-13 * 2, 'k_d4': 0.006 * 3.8,
-                              'kpu': 0.00095,
-                              'ka2': 4.98e-13 * 2.45, 'kd4': 0.3 * 2.867,
-                              'kint_a': 0.000124, 'kint_b': 0.00086,
-                              'krec_a1': 0.0028, 'krec_a2': 0.01, 'krec_b1': 0.005, 'krec_b2': 0.05}
-        dual_parameters = {'kint_a': 0.00052, 'kSOCSon': 6e-07, 'kint_b': 0.00052, 'krec_a1': 0.001, 'krec_a2': 0.1,
-                           'krec_b1': 0.005, 'krec_b2': 0.05}
-        scale_factor = 1.227
-
-        Mixed_Model = DualMixedPopulation('Mixed_IFN_ppCompatible', 0.8, 0.2)
-        Mixed_Model.model_1.set_parameters(initial_parameters)
-        Mixed_Model.model_1.set_parameters(dual_parameters)
-        Mixed_Model.model_1.set_parameters({'R1': 12000.0, 'R2': 1511.1})
-        Mixed_Model.model_2.set_parameters(initial_parameters)
-        Mixed_Model.model_2.set_parameters(dual_parameters)
-        Mixed_Model.model_2.set_parameters({'R1': 6755.56, 'R2': 1511.1})
-
-        # Prepare model for reset
-        Mixed_Model.model_1.default_parameters = Mixed_Model.model_1.parameters
-        Mixed_Model.model_2.default_parameters = Mixed_Model.model_2.parameters
-
-        # Fold changes to test over
-        fold_changes = list(np.logspace(-1, 4))
-
-        def curve_builder(fold_change):
-            time_list = list(linspace(2.5, 60.0, num=15))
-            alpha_dose_list = list(logspace(-2, 5, num=35))
-            beta_dose_list = list(logspace(-2, 4, num=35))
-
-            fold_change_param_dict = {#'kd3': Mixed_Model.model_1.parameters['kd3'] * fold_change,
-                                      'kd4': Mixed_Model.model_1.parameters['kd4'] * fold_change,
-                                      #'k_d3': Mixed_Model.model_1.parameters['k_d3'] * fold_change,
-                                      'k_d4': Mixed_Model.model_1.parameters['k_d4'] * fold_change}
-
-            temp1 = Mixed_Model.mixed_dose_response(time_list, 'TotalpSTAT', 'Ia', alpha_dose_list,
-                                                             parameters=dict({'Ib': 0}, **fold_change_param_dict),
-                                                             sf=scale_factor)
-            alpha_response = IfnData(name='custom', df=temp1)
-            temp2 = Mixed_Model.mixed_dose_response(time_list, 'TotalpSTAT', 'Ib', beta_dose_list,
-                                                             parameters=dict({'Ia': 0}, **fold_change_param_dict),
-                                                             sf=scale_factor)
-            beta_response = IfnData(name='custom', df=temp2)
-            Mixed_Model.reset_global_parameters()
-            alpha_ec50 = alpha_response.get_ec50s()['Cytokine'][-1][1]
-            beta_ec50 = beta_response.get_ec50s()['Cytokine'][-1][1]
-
-
-            # For debugging purposes only
-            # Put the next line above "def curve_builder():"
-            #fig = DoseresponsePlot((1,1))
-            #fig.add_trajectory(alpha_response, 60.0, 'plot', '-', (0,0), 'Cytokine', label=fold_change, linewidth=np.random.random())
-            #fig.add_trajectory(beta_response, 60.0, 'plot', '-', (0,1), 'Cytokine', label=fold_change, linewidth=np.random.random())
-            # Put the next line after "beta_curve.append(x2)"
-            #fig.show_figure()
-
-            return alpha_ec50, beta_ec50
-
-        alpha_curve = []
-        beta_curve = []
-        for f in fold_changes:
-            x1, x2 = curve_builder(f)
-            alpha_curve.append(x1)
-            beta_curve.append(x2)
-
-        # Set up plot
-        ec50_plot, ec50_axes = plt.subplots(1, 1, figsize=(7.5, 6))
-        ec50_axes.set_xlabel("Fold change in K3 & K4")
-        ec50_axes.set_ylabel(r"$EC_{50}$ (pM)")
-        plt.suptitle(r"EC50 at 60 minutes")
-        ec50_axes.set_yscale('log')
-        ec50_axes.set_xscale('log')
-        ec50_axes.plot(fold_changes, alpha_curve, color=alpha_palette[5], linewidth=2.0, label=r'IFN$\alpha$')
-        ec50_axes.plot(fold_changes, beta_curve, color=beta_palette[5], linewidth=2.0, label=r'IFN$\beta$')
-        plt.legend()
-        ec50_plot.savefig(os.path.join(os.getcwd(), 'results', 'Figures', 'Figure_4', 'Figure_4_EC50_vs_K3_and_K4.pdf'))
-
-
-    # ----------------------------------------------------
-    # Plot
-    # ----------------------------------------------------
-    increase_K4_figure()
-    #ec50_vs_K3_figure()
+    K4_flag = False
+    TC_flag = True
+    # ---------------------------------------------
+    # ---------------------------------------------
+    if K4_flag:
+        increase_K4_figure()
+    if TC_flag:
+        timecourse_figure()
