2f7466a23bac3e42741045071fbb5450b19aae4d
dakirby <39995281+dakirby@users.noreply.github.com>
2021-03-01 12:02:22 -0500

    updated makelins.sh to work for general directory locations

--- makelinks.sh
+++ makelinks.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-rm ~/Documents/IFNmodeling/ifnscripts/ifnclass
-rm ~/Documents/IFNmodeling/ifnscripts/ifnmodels
-rm ~/Documents/IFNmodeling/ifnscripts/ifndatabase
+rm "$DIR/ifnscripts/ifnclass"
+rm $DIR/ifnscripts/ifnmodels
+rm $DIR/ifnscripts/ifndatabase
 
-ln -s ~/Documents/IFNmodeling/ifndatabase ~/Documents/IFNmodeling/ifnscripts/ifndatabase 
-ln -s ~/Documents/IFNmodeling/ifnmodels ~/Documents/IFNmodeling/ifnscripts/ifnmodels
-ln -s ~/Documents/IFNmodeling/ifnclass ~/Documents/IFNmodeling/ifnscripts/ifnclass
+ln -s $DIR/ifndatabase $DIR/ifnscripts/ifndatabase
+ln -s $DIR/ifnmodels $DIR/ifnscripts/ifnmodels
+ln -s $DIR/ifnclass $DIR/ifnscripts/ifnclass
 
-rm ~/Documents/IFNmodeling/pydream/GAB/ifndatabase
-rm ~/Documents/IFNmodeling/pydream/GAB/ifnmodels
-rm ~/Documents/IFNmodeling/pydream/GAB/ifnclass
+rm $DIR/pydream/GAB/ifndatabase
+rm $DIR/pydream/GAB/ifnmodels
+rm $DIR/pydream/GAB/ifnclass
 
-ln -s ~/Documents/IFNmodeling/ifndatabase ~/Documents/IFNmodeling/pydream/GAB/ifndatabase 
-ln -s ~/Documents/IFNmodeling/ifnmodels ~/Documents/IFNmodeling/pydream/GAB/ifnmodels
-ln -s ~/Documents/IFNmodeling/ifnclass ~/Documents/IFNmodeling/pydream/GAB/ifnclass
+ln -s $DIR/ifndatabase $DIR/pydream/GAB/ifndatabase
+ln -s $DIR/ifnmodels $DIR/pydream/GAB/ifnmodels
+ln -s $DIR/ifnclass $DIR/pydream/GAB/ifnclass
 
-rm ~/Documents/IFNmodeling/ifnscripts/NIH/ifndatabase 
-rm ~/Documents/IFNmodeling/ifnscripts/NIH/ifnmodels
-rm ~/Documents/IFNmodeling/ifnscripts/NIH/ifnclass
+rm $DIR/ifnscripts/NIH/ifndatabase
+rm $DIR/ifnscripts/NIH/ifnmodels
+rm $DIR/ifnscripts/NIH/ifnclass
 
-ln -s ~/Documents/IFNmodeling/ifndatabase ~/Documents/IFNmodeling/ifnscripts/NIH/ifndatabase 
-ln -s ~/Documents/IFNmodeling/ifnmodels ~/Documents/IFNmodeling/ifnscripts/NIH/ifnmodels
-ln -s ~/Documents/IFNmodeling/ifnclass ~/Documents/IFNmodeling/ifnscripts/NIH/ifnclass
+ln -s $DIR/ifndatabase $DIR/ifnscripts/NIH/ifndatabase
+ln -s $DIR/ifnmodels $DIR/ifnscripts/NIH/ifnmodels
+ln -s $DIR/ifnclass $DIR/ifnscripts/NIH/ifnclass
 
-rm ~/Documents/IFNmodeling/ifnscripts/EpoR/ifndatabase 
-rm ~/Documents/IFNmodeling/ifnscripts/EpoR/ifnmodels
-rm ~/Documents/IFNmodeling/ifnscripts/EpoR/ifnclass
+rm $DIR/ifnscripts/EpoR/ifndatabase
+rm $DIR/ifnscripts/EpoR/ifnmodels
+rm $DIR/ifnscripts/EpoR/ifnclass
 
-ln -s ~/Documents/IFNmodeling/ifndatabase ~/Documents/IFNmodeling/ifnscripts/EpoR/ifndatabase 
-ln -s ~/Documents/IFNmodeling/ifnmodels ~/Documents/IFNmodeling/ifnscripts/EpoR/ifnmodels
-ln -s ~/Documents/IFNmodeling/ifnclass ~/Documents/IFNmodeling/ifnscripts/EpoR/ifnclass
+ln -s $DIR/ifndatabase $DIR/ifnscripts/EpoR/ifndatabase
+ln -s $DIR/ifnmodels $DIR/ifnscripts/EpoR/ifnmodels
+ln -s $DIR/ifnclass $DIR/ifnscripts/EpoR/ifnclass
