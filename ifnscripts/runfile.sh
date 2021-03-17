7c87cfca45c52b5c1c1b96d9521c4e49212e9077
dakirby <39995281+dakirby@users.noreply.github.com>
2021-03-17 13:54:39 -0400

    gave runfile.sh optional argument for fiting

--- ifnscripts/runfile.sh
+++ ifnscripts/runfile.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python $DIR/pydream/GAB/run_PyDREAM.py
+while getopts ":a" opt; do
+  case $opt in
+  fit)
+      python $DIR/pydream/GAB/run_PyDREAM.py
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
 
 python $DIR/figure_2.py
 python $DIR/figure_3.py
